19de40a9ce8453e32bebd32751e25b9995365cfd
Leimerm <matthewleimer@gmail.com>
2020-07-13 18:48:55 -0500

    Try to push

---
+++ newcar.rb
@@ -0,0 +1,43 @@
+class Car
+
+@@array = Array.new
+
+def self.all_instances
+    @@array
+end
+
+attr_reader :year, :make, :model, :color
+
+def initialize (year, make, model, color)
+    @year=year
+    @make=make.capitalize
+    @model=model
+    @color=color.capitalize
+    @@array << self 
+end
+
+def to_s
+    "#{@year} #{@make} #{@model} COLOR:#{@color.rjust(20)}"
+end
+
+end
+
+car1=Car.new(2004, "mozda", 6, "red")
+car2=Car.new(2009, "mozda", 3, "blue")
+car3=Car.new(2012, "mozda", 6, "red")
+car4=Car.new(2005, "hundya", "Sonata", "white")
+car5=Car.new(2001, "hundya", "Elantra", "silver")
+car6=Car.new(2018, "hundya", "Sonata", "black")
+puts "Cool Used Car Lot Inventory".center(50, '.')
+puts Car.all_instances
+
+puts "What brand of car are you looking for?"
+answer=gets.chomp
+if answer.include? Car@model
+    puts "We have this in stock!"
+else
+    puts "Sorry we do not have this model in stock."
+end
+
+
+
