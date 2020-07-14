241a3b38be28950d773111a89026bfdfb800de42
Leimerm <matthewleimer@gmail.com>
2020-07-13 20:15:29 -0500

    Array

--- newcar.rb
+++ newcar.rb
@@ -1,19 +1,19 @@
 class Car
 
-@@array = Array.new
+#@@array = Array.new
 
-def self.all_instances
-    @@array
-end
+#def self.all_instances
+    #@@array
+#end
 
-attr_reader :year, :make, :model, :color
+attr_accessor :year, :make, :model, :color
 
 def initialize (year, make, model, color)
     @year=year
     @make=make.capitalize
     @model=model
     @color=color.capitalize
-    @@array << self 
+    #@@array << self 
 end
 
 def to_s
@@ -22,22 +22,44 @@ end
 
 end
 
-car1=Car.new(2004, "mozda", 6, "red")
-car2=Car.new(2009, "mozda", 3, "blue")
-car3=Car.new(2012, "mozda", 6, "red")
-car4=Car.new(2005, "hundya", "Sonata", "white")
-car5=Car.new(2001, "hundya", "Elantra", "silver")
-car6=Car.new(2018, "hundya", "Sonata", "black")
+car1=Car.new("2004", "mozda", "6", "red")
+car2=Car.new("2009", "mozda", "3", "blue")
+car3=Car.new("2012", "mozda", "6", "red")
+car4=Car.new("2005", "hundya", "Sonata", "white")
+car5=Car.new("2001", "hundya", "Elantra", "silver")
+car6=Car.new("2018", "hundya", "Sonata", "black")
 puts "Cool Used Car Lot Inventory".center(50, '.')
-puts Car.all_instances
+#puts Car.all_instances
 
-puts "What brand of car are you looking for?"
+stock = [car1, car2]
+puts "Enter color"
 answer=gets.chomp
-if answer.include? Car@model
-    puts "We have this in stock!"
-else
-    puts "Sorry we do not have this model in stock."
+
+puts "Enter model"
+answer1=gets.chomp
+
+
+r =[]
+stock.each do |car|  
+    if car.color == answer.capitalize
+    r.push()
+    end
+    if car.model == answer1.capitalize
+        puts "yes"
+    end
 end
+
+
+
+
+
+# puts "What brand of car are you looking for?"
+# answer=gets.chomp
+# if stock.include? answer
+#     puts "We have this in stock!"
+# else
+#     puts "Sorry we do not have this model in stock."
+# end
 
 
 
